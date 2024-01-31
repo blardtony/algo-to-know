09504811c72b8757eeed81c05b7c9ae85f96fa72
Tony Blard <tonyblard55@gmail.com>
2024-01-31 15:11:26 +0100

    Feat : Create insertion sort

---
+++ sorting/insertion.c
@@ -0,0 +1,16 @@
+#include  "insertion.h"
+
+void insertion_sort(int *array, size_t size)
+{
+    for (size_t i = 1; i < size; i++)
+    {
+        int key = array[i];
+        int j = i - 1;
+        while (j >= 0 && key < array[j])
+        {
+            array[j + 1] = array[j];
+            j--;
+        }
+        array[j + 1] = key;
+    }
+}
---
+++ sorting/insertion.h
@@ -0,0 +1,9 @@
+#ifndef INSERTION_H
+#define INSERTION_H
+
+#include <stddef.h>
+#include <stdlib.h>
+
+void insertion_sort(int *array, size_t size);
+
+#endif /* !INSERTION_SORT_H */
